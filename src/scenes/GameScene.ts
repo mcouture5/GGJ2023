609b0613ae2665dcd7cab62cf5c82c21a0784812
Benjamin Dodge <benjamin9dodge@gmail.com>
2023-02-03 22:46:55 +0000

    Added theme music and walking sound.

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -11,6 +11,9 @@ export class GameScene extends Phaser.Scene {
     private plantLayer: PlantLayer;
     private dogLayer: DogLayer;
 
+    // sounds
+    private music: Phaser.Sound.BaseSound;
+
     constructor() {
         super({
             key: 'GameScene'
@@ -18,6 +21,20 @@ export class GameScene extends Phaser.Scene {
     }
 
     create(): void {
+        // start playing music if not already playing. fade it in.
+        if (!this.music || !this.music.isPlaying) {
+            let startVolume: number = 0.1;
+            let endVolume: number = 0.2;
+            this.music = this.sound.add('ruff-ruff-root', {loop: true, volume: startVolume});
+            this.music.play();
+            this.add.tween({
+                targets: this.music,
+                volume: {from: startVolume, to: endVolume},
+                ease: 'Linear',
+                duration: 1300
+            });
+        }
+
         this.farmLayer = new FarmLayer(this);
         this.add.existing(this.farmLayer);
         this.farmLayer.create();
