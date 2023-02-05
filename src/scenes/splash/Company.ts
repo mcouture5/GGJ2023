f1a667e6a9152c49de3dadb80b7f9563f193a5be
Benjamin Dodge <benjamin9dodge@gmail.com>
2023-02-05 06:00:31 +0000

    Added awoo sound to splash screen.

--- src/scenes/splash/Company.ts
+++ src/scenes/splash/Company.ts
@@ -4,15 +4,22 @@ const { r, g, b } = BACKGROUND_RBG;
 
 export class Company extends Phaser.Scene {
 
+    private awooSound: Phaser.Sound.BaseSound;
+
     constructor() {
         super({
             key: 'Company'
         });
     }
+
     preload() {
         this.cameras.main.setBackgroundColor(BACKGROUND_COLOR);
     }
+
     create() {
+        this.awooSound = this.sound.add('awoo', {volume: 0.2});
+        this.awooSound.play();
+
         setTimeout(() => {
             this.add.sprite(this.cameras.main.width / 2, this.cameras.main.height / 2, 'company').setOrigin(0.5, 0.5);
             this.cameras.main.fadeIn(1250, r, g, b);
@@ -24,6 +31,7 @@ export class Company extends Phaser.Scene {
         });
         this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
             setTimeout(() => {
+                this.awooSound && this.awooSound.stop();
                 this.scene.start('PhaserSplash');
             }, 500);
         });
